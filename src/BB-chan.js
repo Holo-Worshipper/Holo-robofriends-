457055cfd27cf1c84b79a5ba25e9f5c28a6f0c3e
Holo-Worshipper <timidtapioca@gmail.com>
2019-05-02 15:51:18 +0800

    I'm committed.

--- src/BB-chan.js
+++ src/BB-chan.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { robots } from './Robots.js';
 import CardList from './CardList.js';
 import SearchBox from './SearchBox.js'
 
-const BB = (props) => {
-    return(
-        <div>
-            <h1>Robofriends</h1>
-            <SearchBox />
-            <CardList robots = {robots}  />
-        </div>
-    );
+const state = {
+    robots: robots,
+    searchfield: "",
 }
+
+class BB extends Component {
+    render() {
+        return(
+            <div className = "tc">
+                <h1>Robofriends</h1>
+                <SearchBox />
+                <CardList robots = {robots}  />
+            </div>
+        );
+    };
+};
 
 export default BB;
