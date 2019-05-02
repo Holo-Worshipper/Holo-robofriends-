e0e57223d22dfd64047ea919fbc05f3ed0007d7f
Holo-Worshipper <timidtapioca@gmail.com>
2019-05-02 15:31:33 +0800

    I'm committed.

--- src/BB-chan.js
+++ src/BB-chan.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { robots } from './Robots.js';
 import CardList from './CardList.js';
+import SearchBox from './SearchBox.js'
 
 const BB = (props) => {
     return(
         <div>
             <h1>Robofriends</h1>
+            <SearchBox />
             <CardList robots = {robots}  />
         </div>
     );
