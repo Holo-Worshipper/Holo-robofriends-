87acc7fa90d630c3acde65b24402311b20e3337d
Holo-Worshipper <timidtapioca@gmail.com>
2019-05-02 14:05:49 +0800

    I'm committed.

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,9 @@
  import React from 'react';
+ import 'tachyons';
 
  const Card = (props) => {
      return(
-        <div>
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img src="https://robohash.org/test?200x200" alt="" />
             <div>
                 <h2>BB</h2>
