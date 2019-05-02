3177831fb28c2899cfdfa95cfb17cd43334423b4
Holo-Worshipper <timidtapioca@gmail.com>
2019-05-02 14:01:29 +0800

    I'm committed.

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+ import React from 'react';
+
+ const Card = (props) => {
+     return(
+        <div>
+            <img src="https://robohash.org/test?200x200" alt="" />
+            <div>
+                <h2>BB</h2>
+                <p>BB-chan@BBChannel.com</p>
+            </div>
+        </div>
+     )
+ }
+
+ export default Card;
