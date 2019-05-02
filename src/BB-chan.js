52c7354260467c216d843bbe555a886fd008bab1
Holo-Worshipper <timidtapioca@gmail.com>
2019-05-02 16:21:20 +0800

    I'm committed.

--- src/BB-chan.js
+++ src/BB-chan.js
@@ -3,18 +3,28 @@ import { robots } from './Robots.js';
 import CardList from './CardList.js';
 import SearchBox from './SearchBox.js'
 
-const state = {
-    robots: robots,
-    searchfield: "",
-}
-
 class BB extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            robots: robots,
+            searchfield: "",
+        }
+    }
+
+    onSearchChange(event) {
+        console.log(event.target.value);
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase);
+        })
+    }
+
     render() {
         return(
             <div className = "tc">
                 <h1>Robofriends</h1>
-                <SearchBox />
-                <CardList robots = {robots}  />
+                <SearchBox searchChange = {this.onSearchChange}/>
+                <CardList robots = {this.state.robots}  />
             </div>
         );
     };
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,9 +1,9 @@
  import React from 'react'; 
 
- const SearchBox = () => {
+ const SearchBox = ({ searchChange }) => {
      return(
          <div className="pa2">
-             <input className = "pa3 ba b--green bg-lightest-blue" type="search" placeholder="search robots" />
+             <input onChange = {searchChange} className = "pa3 ba b--green bg-lightest-blue" type="search" placeholder="search robots" />
          </div>
      )
 }
