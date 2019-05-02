084abd1a7bb27f5f9db259001895d275aee64e38
Holo-Worshipper <timidtapioca@gmail.com>
2019-05-02 13:16:01 +0800

    I'm committed.

---
+++ src/BBchan.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './BB.css'
+
+class BB extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                <p>nothing so far!</p>
+                <p>Welcome to React!</p>
+            </div>
+        )
+    }
+}
+
+export default BB
